4fe5219a3ef76520110c72f2d7bf4acca0d76019
qalicha-dev28 <qalichanajma44@gmail.com>
2025-06-26 22:14:52 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// Create a new paragraph element
+const newHeader = document.createElement('h1');
+
+// Set the id attribute
+newHeader.id = 'victory';
+
+// Set the text content
+newHeader.textContent = 'YOUR-NAME is the champion'; // Replace YOUR-NAME with your name
+
+// Append to the DOM
+document.body.appendChild(newHeader);
+
+// Remove the main element
+const main = document.getElementById('main');
+main.remove();
